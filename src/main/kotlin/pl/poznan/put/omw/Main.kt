1fa7b76c5d1f410434f62e0f3a1302e1d70af9a0
Witold Kupś <witold.kups@interlan.pl>
2020-02-15 08:26:07 +0100

    Add websocket initial integration

--- src/main/kotlin/pl/poznan/put/omw/Main.kt
+++ src/main/kotlin/pl/poznan/put/omw/Main.kt
@@ -3,9 +3,9 @@ package pl.poznan.put.omw
 import kotlinx.serialization.SerializationStrategy
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
+import mu.KLogging
+import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 
 
@@ -69,3 +69,44 @@ private fun Request.Builder.auth(authResult: AuthorizationResponse) = header("Au
 
 private inline fun <reified T> T.toRequest(serializer: SerializationStrategy<T>, json: Json) =
         json.stringify(serializer, this).toRequestBody(JSON_MEDIA_TYPE)
+
+private fun OkHttpClient.manageWebSocket(uciServerConfig: UciServerConfig, authResult: AuthorizationResponse) {
+    val request = request {
+        url(uciServerConfig.url + WEBSOCKET_PATH).auth(authResult)
+    }
+    val listener = UciWebSocketListener(uciServerConfig.engine)
+    newWebSocket(request, listener)
+    dispatcher.executorService.shutdown()
+}
+
+class UciWebSocketListener(
+        private val engine: UciServerEngine
+) : WebSocketListener() {
+    private companion object : KLogging()
+
+    override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+        logger.info("socket closed: ($code) $reason")
+    }
+
+    override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+        logger.info("closing socket: ($code) $reason")
+        webSocket.close(100, null)
+    }
+
+    override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+        logger.error(t) { "connection with websocket failure: $response" }
+    }
+
+    override fun onMessage(webSocket: WebSocket, text: String) {
+        logger.info("message: $text")
+    }
+
+    override fun onOpen(webSocket: WebSocket, response: Response) {
+        logger.info("socket opened with response $response")
+        engine.options.forEach { (option, value) ->
+            webSocket.send("setoption name $option value $value")
+        }
+        webSocket.send("position fen r4rk1/pp5p/2p2ppB/3pP3/2P2Q2/P1N2P2/1q4PP/n4R1K w - - 0 21")
+        webSocket.send("go depth 30")
+    }
+}
