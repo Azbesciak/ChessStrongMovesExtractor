a2ad55e7141cc81aba2f6eeb2e044d0303c54f03
Witold Kupś <witold.kups@interlan.pl>
2020-02-15 08:25:45 +0100

    Migrate to okhttp3

--- src/main/kotlin/pl/poznan/put/omw/Main.kt
+++ src/main/kotlin/pl/poznan/put/omw/Main.kt
@@ -1,79 +1,71 @@
 package pl.poznan.put.omw
 
-import io.ktor.client.HttpClient
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.json.serializer.KotlinxSerializer
-import io.ktor.client.request.HttpRequestBuilder
-import io.ktor.client.request.headers
-import io.ktor.client.request.post
-import io.ktor.client.request.url
-import io.ktor.http.ContentType
-import io.ktor.http.contentType
-import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.SerializationStrategy
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+
 
 const val AUTH_PATH = "/user/login"
 const val ENGINE_START_PATH = "/engine/start"
 const val WEBSOCKET_PATH = "/ws_engine"
+val JSON_MEDIA_TYPE = "application/json; charset=utf-8".toMediaType()
 
 fun main(args: Array<String>) = ProgramExecutor {
     val game = ChessBoardReader.getGames(inputPath)
     val mainPathMovesGame = VariantMovesFilter.filter(game)
+    val client = OkHttpClient()
     val json = Json(JsonConfiguration.Stable)
     val uciServerConfig = readServerConfig(json)
     println(uciServerConfig)
     println(mainPathMovesGame)
-    runBlocking {
-        HttpClient {
-            install(JsonFeature) {
-                serializer = KotlinxSerializer(json)
-            }
-        }.use {
-            it.connectWithServer(uciServerConfig)
-        }
-    }
+    val authResult = client.authorize(uciServerConfig, json)
+    client.startEngine(uciServerConfig, authResult, json)
 }.main(args)
 
 private fun Params.readServerConfig(json: Json) =
         ServerConfigReader(json) read uciServerConfigPath
 
-private suspend fun HttpClient.connectWithServer(uciServerConfig: UciServerConfig) {
-    val authResult: AuthorizationResponse = authorize(uciServerConfig)
-    startEngine(uciServerConfig, authResult)
-}
-
-private suspend fun HttpClient.authorize(uciServerConfig: UciServerConfig): AuthorizationResponse {
+private fun OkHttpClient.authorize(uciServerConfig: UciServerConfig, json: Json): AuthorizationResponse {
     val credentials = Credentials(uciServerConfig.login, uciServerConfig.password)
-    val authResult: AuthorizationResponse = post {
-        request(uciServerConfig, AUTH_PATH)
-        body = credentials
+    val body = credentials.toRequest(Credentials.serializer(), json)
+    val request = request {
+        url(uciServerConfig.url + AUTH_PATH).post(body)
     }
-    require(authResult.success) { "Authorization failed for $credentials" }
-    return authResult
-}
-
-private suspend fun HttpClient.startEngine(uciServerConfig: UciServerConfig, authResult: AuthorizationResponse) {
-    val startResult: EngineStartCommandResponse = post {
-        request(uciServerConfig, ENGINE_START_PATH, authResult)
-        body = EngineStartCommand(uciServerConfig.engine.name)
-    }
-    require(startResult.success) {
-        val info = if (startResult.info.isNotBlank()) ": ${startResult.info}" else ""
-        "Could not start engine$info"
+    return execute(request) {
+        val authResult = json.parse(AuthorizationResponse.serializer(), it)
+        require(authResult.success) { "Authorization failed for $credentials" }
+        authResult
     }
 }
 
-private fun HttpRequestBuilder.request(
-        uciServerConfig: UciServerConfig,
-        path: String,
-        auth: AuthorizationResponse? = null
-) {
-    url(uciServerConfig.url + path)
-    contentType(ContentType.Application.Json)
-    auth?.let {
-        headers {
-            set("Authorization", "Bearer ${auth.token}")
+private fun OkHttpClient.startEngine(uciServerConfig: UciServerConfig, authResult: AuthorizationResponse, json: Json) {
+    val body = EngineStartCommand(uciServerConfig.engine.name).toRequest(EngineStartCommand.serializer(), json)
+    val request = request {
+        url(uciServerConfig.url + ENGINE_START_PATH)
+                .auth(authResult)
+                .post(body)
+    }
+    return execute(request) {
+        val startResult = json.parse(EngineStartCommandResponse.serializer(), it)
+        require(startResult.success) {
+            val info = if (startResult.info.isNotBlank()) ": ${startResult.info}" else ""
+            "Could not start engine$info"
         }
     }
 }
+
+private inline fun <reified T> OkHttpClient.execute(request: Request, onResponse: (String) -> T) =
+        newCall(request).execute().use {
+            onResponse(requireNotNull(it.body).string())
+        }
+
+private fun request(builder: Request.Builder.() -> Request.Builder) = Request.Builder().builder().build()
+
+private fun Request.Builder.auth(authResult: AuthorizationResponse) = header("Authorization", "Bearer ${authResult.token}")
+
+private inline fun <reified T> T.toRequest(serializer: SerializationStrategy<T>, json: Json) =
+        json.stringify(serializer, this).toRequestBody(JSON_MEDIA_TYPE)
